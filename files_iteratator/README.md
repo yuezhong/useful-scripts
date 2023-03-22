dd3e8053202d0ee2398dbe08aafc60fc829a3dfd
yuezhong <yuezhongl@gmail.com>
2023-03-22 19:51:35 +0800

    Added Files iterator script

---
+++ files_iteratator/README.md
@@ -0,0 +1,2 @@
+Script to iterate files in a folder then process them.
+This is just a template. Replace the line with "cat" command with whatever processing is required.
---
+++ files_iteratator/iterate_files.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# template script to iterate through files in a folder
+
+# path to files
+FILES=$1
+
+for f in $FILES
+do
+    echo "Processing $f file..."
+    # Do something. Just cat'ing the filename out for now
+    cat "$f"
+
+done
