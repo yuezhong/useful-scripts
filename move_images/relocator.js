14e5d64a2c017ab9e6e3bc5a51f761f47e06515e
yuezhong <yuezhongl@gmail.com>
2023-08-14 21:51:49 +0800

    Fixed bug on string positions
    Updated string positions for extraction and also removed leading 0 for single digit months

--- move_images/relocator.js
+++ move_images/relocator.js
@@ -9,7 +9,8 @@ const srcDir = path.join(__dirname, 'source');
 const destDir = path.join(__dirname, 'moved');
 
 // Months Array
-const months = ['01-January',
+const months = ['unknown',
+                '01-January',
                 '02-February',
                 '03-March',
                 '04-April',
@@ -34,24 +35,29 @@ fs.readdir(srcDir, function(err, files) {
     let regex = new RegExp(/[^\\_-][^\\_-]*[_-]20[0-9][0-9][01][0-9][0-3][0-9][_-][^\\]*/);
 
     files.forEach((item) => {
-        //console.warn("File name is: ", item);
+        console.warn("File name is: ", item);
         if(regex.test(item) ) {
-            console.log(item);
-            let pos = item.indexOf("_20");
+            let pos = item.search("_20");
             let year = item.slice(pos+1, pos+5);
-            let month = item.slice(pos+6, pos+7);
+            let month = item.slice(pos+5,pos+7);
+
+            // Get rid of leading zero for months prior to october
+            if(month.at(0) == 0) {
+                month = month.slice(1);
+            }
+
             // Create Year folder
             if(!fs.existsSync(`${destDir}/${year}`)) {
                     fs.mkdirSync(`${destDir}/${year}`);
             }
             // Create month folder
-            if(!fs.existsSync(`${destDir}/${year}/${months[month - 1]}`)) {
-                fs.mkdirSync(`${destDir}/${year}/${months[month - 1]}`);
+            if(!fs.existsSync(`${destDir}/${year}/${months[month]}`)) {
+                fs.mkdirSync(`${destDir}/${year}/${months[month]}`);
             }
 
             // Copy files from original folder to the destination folder
-            fs.copyFileSync(`${srcDir}/${item}`, `${destDir}/${year}/${months[month - 1]}/${item}`);
-            console.log("year: " + year + " month: " + months[month - 1]);
+            fs.copyFileSync(`${srcDir}/${item}`, `${destDir}/${year}/${months[month]}/${item}`);
+            console.log("year: " + year + " month: " +  month + " " + months[month]);
         }
 
     });
